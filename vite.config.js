2ed40dcf8ac3ba36aa61010a67c7017c383078b1
samhanmer <SamHanmer@hotmail.com>
2026-05-13 02:03:59 -0600

    Troubleshooting page navigation try 2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '/SalsArcadeNew/',
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        physical: 'physical.html',
+	digital: 'digital.html',
+        events: 'events.html',
+      }
+    }
+  }
 })
